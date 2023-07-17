8276e187b3ebf15647a6ee1fbfc44d1cd8fb6c3d
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-17 17:13:09 +0530

    firebase added

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String apiKey = "AIzaSyAXJNSJ5XhZ6t5OxZj7RNkKaBeMleADyd0";
+  static String appId = "1:458310306952:web:8733db7c724bf6fcdc233d";
+  static String messagingSenderId = "458310306952";
+  static String projectId = "chatappflutter-a7d38";
+}
