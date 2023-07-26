ce1d11cbaacb5fe9fb353e23a63ee5f3993ae741
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-26 19:35:12 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:groupie/helper/helper_functions.dart';
 import 'package:groupie/pages/login.dart';
-import 'package:groupie/shared/constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: FirebaseOptions(
-            apiKey: Constants.apiKey,
-            appId: Constants.appId,
-            messagingSenderId: Constants.messagingSenderId,
-            projectId: Constants.projectId));
-  } else {
-    await Firebase.initializeApp();
-  }
-
+  // if (kIsWeb) {
+  //   await Firebase.initializeApp(
+  //       options: FirebaseOptions(
+  //           apiKey: Constants.apiKey,
+  //           appId: Constants.appId,
+  //           messagingSenderId: Constants.messagingSenderId,
+  //           projectId: Constants.projectId));
+  // } else {
+  //   await Firebase.initializeApp();
+  // }
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
